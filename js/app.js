83d1533c11f05cc6960a1099c4c7bfd20d7a1923
amperscript <ampersand@internet.ru>
2024-07-14 13:59:44 +0300

    Добавлена стилизация, получение даты

--- js/app.js
+++ js/app.js
@@ -2,33 +2,51 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const inputTitle = document.getElementById("note-title");
   const inputDescription = document.getElementById("note-content");
-  const title = document.getElementById("title");
-  const noteTitle = document.createElement("h2");
-  const sumbitBtn = document.createElement("sumbit-btn");
+  const submitBtn = document.getElementById("submit-btn");
+  const notesList = document.getElementById("notes");
+  const addBtn = document.getElementById("add-btn");
+  const modalNoteContainer = document.getElementById("modal-container");
 
-  const note = document.createElement("div");
+  const currentDate = new Date();
 
-  function handleTextResult() {
-    const noteTitle = document.createElement("h2");
-    noteTitle.innerText = inputTitle.value;
-    return noteTitle;
+  function checkValues(event) {
+    event.preventDefault();
+
+    if (inputTitle.value === "") {
+      alert("Please enter a valid title");
+    } else {
+      const note = document.createElement("li");
+      note.classList.add("note");
+
+      const dateOfNote = document.createElement("li");
+      dateOfNote.classList.add("note-date");
+      dateOfNote.innerText = `${currentDate.getDate()}.${currentDate.getMonth()}.${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}`;
+      note.appendChild(dateOfNote);
+
+
+      const noteTitle = document.createElement("h2");
+      noteTitle.classList.add("note-title");
+      noteTitle.innerText = inputTitle.value;
+      note.appendChild(noteTitle);
+
+      const noteContent = document.createElement("p");
+      noteContent.innerText = inputDescription.value;
+      note.appendChild(noteContent);
+      notesList.appendChild(note);
+
+      inputTitle.value = "";
+      inputDescription.value = "";
+
+      addBtn.style.display = "block";
+      modalNoteContainer.style.display = "none";
+    }
   }
 
-  function checkValues(){
-  event.preventDefault();
+  function openModal(event) {
+    modalNoteContainer.style.display = "flex";
+    addBtn.style.display = "none";
+  }
 
-  if(inputTitle.value === ""){
-    alert("Please enter a valid title");
-  }
-  else {
-    note.classList.add("note");
-    noteTitle.classList.add("note-title");
-    noteTitle.innerText = inputTitle.value;
-    note.appendChild(noteTitle);
-  }
-    inputTitle.value = "";
-  }
-  inputTitle.addEventListener("input", handleTextResult);
-  sumbitBtn.addEventListener("click", checkValues);
-
-})
+  addBtn.addEventListener("click", openModal);
+  submitBtn.addEventListener("click", checkValues);
+});
